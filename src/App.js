f203a60b00b46016612160484aed568af6fb2bb9
angels101 <angelscodex101@gmail.com>
2021-06-08 14:14:35 +0300

    Header completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import Particles from "react-particles-js";
+ 
 import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 
 function  App() {
   return (
+    <>
+    <Particles 
+    className="particles-canvas"
+    params= {{
+      particles: {
+        number: {
+          value: 30,
+          density: {
+            enable: true,
+            value_area: 900,
+          }
+        },
+        shape: {
+          type: "star",
+          stroke: {
+            width: 6,
+            color: "#f9ab00"
+          }
+        }
+      }
+    }}
+    />
     <Navbar />
-    
+    <Header />
+    </>
   );
 }
 
