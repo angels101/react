df8c6653705c5729fa62210ff873fe94485ce594
angels101 <angelscodex101@gmail.com>
2021-08-04 20:12:34 +0300

    minor surgery

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -21,7 +21,7 @@ function Intro() {
         <div className="intro" id="intro">
              <div className="left">
                     <div className="imgContainer">
-                        <img src="assets/ceo.jpg" alt="" />
+                        <img src="assets/profile.png" alt="" />
                     </div>             
                 </div>
              <div className="right">
